2a808672e15657407c83866b9ed85d2f28b941c7
Dale Weber <hybotics.pdx@gmail.com>
2014-09-02 08:56:29 -0700

    Started converting to use Python3 Removed all the ";"

--- Raspberry-Pi/Python/BlunoUSBComm/BlunoUSBComm.py
+++ Raspberry-Pi/Python/BlunoUSBComm/BlunoUSBComm.py
@@ -22,125 +22,124 @@
 '''
 import serial
 
-EOL = "\r";
-command = "";
+EOL = "\r"
+command = ""
 
 # Set the SSC-32 port - "COM1:" for Windows, "/dev/ttyS0" for Linux
-SSC32_Port = "/dev/ttyS0";
+blunoPort = "/dev/ttyACM0"
 
-LeftMChan = 0;
-LeftMStop = 1520;
+LeftMChan = 0
+LeftMStop = 1520
 
-RightMChan = 1;
-RightMStop = 1525;
+RightMChan = 1
+RightMStop = 1525
 
-PanChan = 2;
-PanHome = 1530;
-PanREnd = 2150;
-PanLEnd = 900;
+PanChan = 2
+PanHome = 1530
+PanREnd = 2150
+PanLEnd = 900
 
-TiltChan = 3;
-TiltHome = 1430;
-TiltUEnd = 500;
-TiltDEnd = 1550;
+TiltChan = 3
+TiltHome = 1430
+TiltUEnd = 500
+TiltDEnd = 1550
 
 # Reads single characters until a CR is read
 def Response (port):
-   ich = "";
-   resp = "";
+   ich = ""
+   resp = ""
 
-   while (ich <> '\r'):
-      ich = port.read(1);
+   while (ich != '\r'):
+      ich = port.read(1)
       
-      if (ich <> '\r'):
-         resp = resp + ich;
-   return resp;
-      
+      if (ich != '\r'):
+         resp = resp + ich
+   return resp
 
 # Converts a servo position in degrees to uS for the SSC-32
 def To_Degrees (uS):
-   result = 0;
+   result = 0
    
-   result = (uS - 1500) / 10;
+   result = (uS - 1500) / 10
    
-   return result;
+   return result
 
 # Converts an SSC-32 servo position in uS to degrees
 def To_uS (degrees):
-   result = 0;
+   result = 0
    
-   result = (degrees * 10) + 1500;
+   result = (degrees * 10) + 1500
    
-   return result;
+   return result
 
 # Wait for a servo move to be completed
 def Wait_for_Servos (port):
    
-   ich = "";
+   ich = ""
    
-   while (ich <> "."):
-      Send_Command (port, "Q", True);
+   while (ich != "."):
+      Send_Command (port, "Q", True)
       
-      ich = port.read(1);
+      ich = port.read(1)
    
-   return;
+   return
 
 # Send EOL to the SSC-32 to start a command executing
 def Send_EOL (port):
 
-   result = port.write(EOL);
+   result = port.write(EOL)
    
-   return result;
+   return result
 
 # Send a command to the SSC-32 with or without an EOL
 def Send_Command (port, cmd, sendeol):
    
-   result = port.write (cmd);
+   result = port.write (cmd)
    
    if (sendeol):
-      Send_EOL (port);
+      Send_EOL (port)
    
-   return result;
+   return result
    
 #   Open the port at 115200 Bps - defaults to 8N1
-ssc32 = serial.Serial(SSC32_Port, 115200);
+bluno = serial.Serial(blunoPort, 115200)
 
 #   Motors All Stop - Left = Ch0, Right = Ch1
-command = "#" + str(LeftMChan) + " P" + str(LeftMStop) + " #" + str(RightMChan) + " P" + str(RightMStop);
-Send_Command (ssc32, command, True);
+command = "#" + str(LeftMChan) + " P" + str(LeftMStop) + " #" + str(RightMChan) + " P" + str(RightMStop)
+Send_Command (bluno, command, True)
 
-Wait_for_Servos (ssc32);
+Wait_for_Servos (bluno)
 
 #   Home the PING Pan/Tilt Base
-command = "#" + str(PanChan) + " P" + str(PanHome) + " #" + str(TiltChan) + " P" + str(TiltHome);
-Send_Command (ssc32, command, True);
+command = "#" + str(PanChan) + " P" + str(PanHome) + " #" + str(TiltChan) + " P" + str(TiltHome)
+Send_Command (bluno, command, True)
 
-Wait_for_Servos (ssc32);
+Wait_for_Servos (bluno)
 
 #   Send the version command
-command = "ver";
-Send_Command (ssc32, command, False);
-Send_EOL (ssc32);
+command = "ver"
+Send_Command (bluno, command, False)
+Send_EOL (bluno)
 
 #   Read the response
-inp = Response(ssc32);
+inp = Response(bluno)
 
 #   Show what we got back
-print inp;
+print (inp)
 
 command = "#" + str(PanChan) + "P1900" + "#" + str(TiltChan) + "P750 T5000"
-Send_Command (ssc32, command, True);
+Send_Command (bluno, command, True)
 
-Wait_for_Servos (ssc32);
+Wait_for_Servos (bluno)
 
 #   Close the port
-ssc32.close();
+bluno.close()
 
 # Test the conversion functions
-print "600 uS = ", To_Degrees(600), " degrees.";
-print "2400 uS = ", To_Degrees(2400), " degrees.";
-print "1500 uS = ", To_Degrees (1500), " degrees.";
+print ("600 uS = %d degrees.", To_Degrees(600))
+print ("2400 uS = %d degrees.", To_Degrees(2400))
+print ("1500 uS = %d degrees.", To_Degrees (1500))
 
-print "-90 degrees = ", To_uS(-90), " uS.";
-print "90 degrees = ", To_uS(90), " uS.";
-print "0 degrees = ", To_uS(0), " uS.";
+print ("-90 degrees = %d uS.", To_uS(-90))
+print ("90 degrees = %d uS.", To_uS(90))
+print ("0 degrees = %d uS.", To_uS(0))
