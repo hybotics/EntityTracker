52b869bc50bf97fe644da54e96c02b56dff99cf9
Dale Weber <hybotics.pdx@gmail.com>
2014-09-01 23:30:14 -0700

    Added BlunoUSBComm script, changed permissions to be executable only for the 	owner on both scripts

---
+++ Raspberry-Pi/Python/BlunoUSBComm/BlunoUSBComm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+	Program:		BlunoUSBComm.py
+	Version:		0.0.1
+	Date:			01-Sep-2014
+	Purpose:		Main script to communicate with the Bluno for services
+
+	Dependencies:	None
+	
+	Copyright (2014) by Dale Weber, hybotics.pdx@gmail.com, @hybotics on App.Net
+	
+	ALL Rights Reserved.
+'''
+
+'''
+	Imports
+'''
+import serial
+
