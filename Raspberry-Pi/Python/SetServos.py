1846fbcacfefc6cce70cb9799cf4b86df14b6880
Dale Weber <hybotics.pdx@gmail.com>
2014-09-01 22:51:39 -0700

    Added new file

---
+++ Raspberry-Pi/Python/SetServos.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+'''
+	Program:		SetServos.py
+	Version:		0.0.1
+	Date:			01-Sep-2014
+	Purpose:		Set Servo center point, offset, and defaults.
+
+	Dependencies:	None
+	
+	Copyright (2014) by Dale Weber, hybotics.pdx@gmail.com, @hybotics on App.Net
+	
+	ALL Rights Reserved.
+'''
+
+'''
+	Set Pan/Tilt servo limits
+'''
+panLeftFull = 500
+panRightFull = 2400
+tiltUpFull = 2000
+tiltDownFull = 500
