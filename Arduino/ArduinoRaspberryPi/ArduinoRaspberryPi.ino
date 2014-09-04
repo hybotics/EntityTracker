f93ad356e453dcd5035594260253b5cf7d7956dd
Dale Weber <hybotics.pdx@gmail.com>
2014-09-03 17:24:42 -0700

    Corrected some spacing issues.

--- Arduino/ArduinoRaspberryPi/ArduinoRaspberryPi.ino
+++ Arduino/ArduinoRaspberryPi/ArduinoRaspberryPi.ino
@@ -6,7 +6,7 @@ void setup() {
 void loop() {
 	char inByte = ' ';
 
-	if(Serial.available()){ // only send data back if data has been sent
+	if (Serial.available()) { // only send data back if data has been sent
 		inByte = Serial.read(); // read the incoming data
 		Serial.println(inByte); // send the data back in a new line so that it is not all one long line
 	}
